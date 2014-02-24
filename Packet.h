b055d8216a72ff89e4b457f8c9de7ffc5888dcf5
ajthompson <ajthompson042@gmail.com>
2014-02-23 19:44:09 -0500

    All currently required classes for Routers are added.  Must still be programmed.

---
+++ Packet.h
@@ -0,0 +1,15 @@
+/**
+ * RouterNode.h
+ *
+ * Header file for a node of a router's queue.
+ * Fields for:
+ */
+#ifndef ROUTERNODE_H
+#define ROUTERNODE_H
+
+class ROUTERNODE_H {
+	// friend class [List class name]
+	
+};
+
+#endif
