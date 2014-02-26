98bccab98b0597c9cf0b9c1e010e8d31272bf93e
ajthompson <ajthompson042@gmail.com>
2014-02-26 11:47:19 -0500

    trying to fix master

---
+++ field.cpp
@@ -0,0 +1,14 @@
+/* 
+* @Author: ajthompson
+* @Date:   2014-02-26 11:17:38
+* @Last Modified by:   ajthompson
+* @Last Modified time: 2014-02-26 11:46:57
+*/
+
+#include <iostream>
+#include "field.h"
+using namespace std;
+
+Field::Field(int dimension, int ) {
+
+}
